d3f50a7e2624ce090c48d2197e7c644dfe6eb8b3
Klaus Wagner <nenioscio@gmail.com>
2019-06-08 02:15:23 +0200

    Complete full implementation of the microbit kitronics board

--- inc/KitronikRobotics.h
+++ inc/KitronikRobotics.h
@@ -27,11 +27,13 @@ DEALINGS IN THE SOFTWARE.
 #define KITRONIKROBOTICS_H
 
 #include "MicroBit.h"
+#include "MicroBitFiber.h"
 
 // MicroBit::flags values
 #define KITRONIKROBOTICS_INITIALIZED                    0x01
 #define KITRONIKROBOTICS_FORWARD                        0x02
 #define KITRONIKROBOTICS_REVERSE                        0x04
+
 
 /**
   * Class definition for a MicroBit device.
@@ -55,26 +57,42 @@ class  KitronikRobotics
     const uint8_t               chip_address; // will be defaulted to 0x6C
     uint8_t                     status;
 
-    uint8_t                     step_init;
-    uint16_t                    step_stage;
-    uint16_t                    stepper_1_steps;
-    uint16_t                    stepper_2_steps;
+    uint16_t                    stepper_steps[2];
+
+    inline uint16_t angle_to_steps(uint16_t angle, uint16_t steps) {
+        return ((angle - 1) * (steps - 1)) / (360-1) + 1;
+    };
+
+    inline uint16_t clamp_u16(uint16_t x, uint16_t upper, uint16_t lower) {
+        return min(upper, max(x, lower));
+    }
 
     public:
 
     /**
       * Constructor.
       *
-      * Create a representation of a MicroBit device, which includes member variables
-      * that represent various device drivers used to control aspects of the micro:bit.
+      * Create a representation of a Kitronik Robotics board device
+      *
+      * @param i2c Refernce to uBit.i2c class
+      * @param chip_address I2C address of the Kitronik Robotics board device
+      *        defaults to 0x6C
+      * @param stepper_1_steps Number os steps required to make a 360 turn on the stepper 1
+      *        defaults to 200
+      * @param stepper_2_steps Number os steps required to make a 360 turn on the stepper 2
+      *        defaults to 200
+      *
+      *
+      * @code
+      * krobotics = new KitronikRobotics(uBit.i2c, 0x6c, 200, 200);
+      * @endcode
       */
-    KitronikRobotics(MicroBitI2C &_i2c, uint8_t _chip_address = 0x6C);
+    KitronikRobotics(MicroBitI2C &_i2c, uint8_t _chip_address = 0x6C, uint16_t stepper_1_steps = 200, uint16_t stepper_2_steps = 200);
 
     /**
       * Post constructor initialisation method.
       *
-      * @return MICROBIT_OK on success. Returns ID of the chip if no communication could be established or
-      *         id of subsequent call.
+      * @return MICROBIT_OK on success. Returns ID of the chip if no communication could be established
       *
       * @code
       * krobotics = new KitronikRobotics(uBit.i2c);
@@ -87,20 +105,69 @@ class  KitronikRobotics
     int init();
 
     /**
+     * Sets the requested servo to the reguested angle.
+     * If the PCA has not yet been initialised calls the initialisation routine.
+     * @param servo Which servo to set
+     * @param degrees the angle to set the servo to
+     *
+     * @code
+     * krobotics->servo_write(1, 90);
+     * @endcode
+     */
+    //% servo.min=1 servo.max=8
+    //% degrees.min=0 degrees.max=180
+    void servo_write(uint8_t servo, uint16_t degrees);
+
+    /**
       * Turn Motor on and set to given rotational speed in given direction
       *
-      * @param motor The id of the motor to start speeding up. 
-      *
+      * @param motor The id of the motor to start speeding up
       * @param direction The direction of the motor rotation (eighter of KROBOTIKROBOTICS_FORWARD or
       *        KROBOTIKROBOTICS_REVERSE)
-      *
       * @param speed The motor speed for the given direction in Percent
       *
       * @code
       * krobotics->motor_on(1, KROBOTIKROBOTICS_FORWARD, 10);
       * @endcode
       */
+    //% motor.min=1 speed.max=4
+    //% speed.min=0 speed.max=100
     void motor_on(uint8_t motor, uint8_t direction, uint16_t speed);
+
+    /**
+     * Turns off the specified motor.
+     * @param motor which motor to turn off
+     */
+    //% motor.min=1 speed.max=4
+    void motor_off(uint8_t motor);
+
+
+    /**
+     * Turns off all motors and servos.
+     */
+    void all_off();
+
+
+    /**
+     * Sets the requested stepper motor to a chosen angle relative to the start position.
+     * if the PCA has not yet been initialised calls the initialisation routine.
+     * @param stepper which stepper motor to turn on
+     * @param direction   which direction to go
+     * @param angle how far to turn the motor relative to start in degrees
+     */
+    //% stepper.min=1 stepper.max=2
+    //% angle.min=1 angle.max=360
+    void stepper_motor_turn_angle(uint8_t stepper, uint8_t direction, uint16_t angle);
+
+    /**
+     * Sets the requested stepper motor to turn a set number of steps.
+     * if the PCA has not yet been initialised calls the initialisation routine.
+     * @param stepper which stepper motor to turn on
+     * @param direction   which direction to go
+     * @param steps how many steps to turn the motor
+     */
+    //% stepper.min=1 stepper.max=2
+    void stepper_motor_turn_steps(uint8_t stepper, uint8_t direction, uint16_t steps);
 };
 
 
--- source/KitronikRobitics.cpp
+++ source/KitronikRobitics.cpp
@@ -25,31 +25,47 @@ DEALINGS IN THE SOFTWARE.
 
 #include "KitronikRobotics.h"
 
+template < typename T >
+T clamp(T value, T min, T max) {
+        return std::max(std::min(value, max), min);
+}
+
 /**
   * Constructor.
   *
-  * Create a representation of a Kitronik Robotics board  device, which includes
-  * member variables that represent various device drivers used to control
-  * aspects of the micro:bit.
+  * Create a representation of a Kitronik Robotics board device
+  *
+  * @param i2c Refernce to uBit.i2c class
+  * @param chip_address I2C address of the Kitronik Robotics board device
+  *        defaults to 0x6C
+  * @param stepper_1_steps Number os steps required to make a 360 turn on the stepper 1
+  *        defaults to 200
+  * @param stepper_2_steps Number os steps required to make a 360 turn on the stepper 2
+  *        defaults to 200
+  *
+  *
+  * @code
+  * krobotics = new KitronikRobotics(uBit.i2c, 0x6c, 200, 200);
+  * @endcode
   */
-KitronikRobotics::KitronikRobotics(MicroBitI2C &_i2c, uint8_t _chip_address) : i2c(_i2c), chip_address(_chip_address << 1), status(0), step_init(0), step_stage(0), stepper_1_steps(200), stepper_2_steps(200)
+KitronikRobotics::KitronikRobotics(MicroBitI2C &_i2c, uint8_t _chip_address, uint16_t stepper_1_steps, uint16_t stepper_2_steps) : i2c(_i2c), chip_address(_chip_address << 1), status(0)
 {
+  stepper_steps[0] = stepper_1_steps;
+  stepper_steps[1] = stepper_2_steps;
 }
 
 /**
   * Post constructor initialisation method.
   *
-  * This call will initialised the scheduler, memory allocator and Bluetooth stack.
-  *
-  * This is required as the Bluetooth stack can't be brought up in a
-  * static context i.e. in a constructor.
+  * @return MICROBIT_OK on success. Returns ID of the chip if no communication could be established
   *
   * @code
-  * kitronik_board.init();
+  * krobotics = new KitronikRobotics(uBit.i2c);
+  * krobotics->init();
   * @endcode
   *
   * @note This method must be called before user code utilises any functionality
-  *       contained by uBit.
+  *       contained by kRobotics device.
   */
 int KitronikRobotics::init()
 {
@@ -75,6 +91,53 @@ int KitronikRobotics::init()
     return MICROBIT_OK;
 }
 
+/**
+ * Sets the requested servo to the reguested angle.
+ * If the PCA has not yet been initialised calls the initialisation routine.
+ * @param servo Which servo to set
+ * @param degrees the angle to set the servo to
+ *
+ * @code
+ * krobotics->servo_write(1, 90);
+ * @endcode
+ */
+//% servo.min=1 servo.max=8
+//% degrees.min=0 degrees.max=180
+void KitronikRobotics::servo_write(uint8_t servo, uint16_t degrees) {
+    uint8_t value_buf[2];
+    uint16_t output_value;
+    uint8_t servo_reg;
+
+    if (!(status & KITRONIKROBOTICS_INITIALIZED))
+        init();
+
+    servo   = clamp(servo, (uint8_t) 1, (uint8_t) 8);
+    degrees = clamp(degrees, (uint16_t) 0, (uint16_t) 180);
+
+    servo_reg    = srv_reg_base + ((servo - 1) * reg_offset);
+    output_value = (degrees * 100 * servo_multiplier) / (10000 + servo_zero_offset);
+    value_buf[0] = output_value & 0xFF;
+    value_buf[1] = (output_value >> 8);
+
+    for (uint8_t i = 0; i < 2; i++) {
+        i2c.writeRegister(chip_address, servo_reg + i, value_buf[i]);
+    }
+}
+
+/**
+  * Turn Motor on and set to given rotational speed in given direction
+  *
+  * @param motor The id of the motor to start speeding up
+  * @param direction The direction of the motor rotation (eighter of KROBOTIKROBOTICS_FORWARD or
+  *        KROBOTIKROBOTICS_REVERSE)
+  * @param speed The motor speed for the given direction in Percent
+  *
+  * @code
+  * krobotics->motor_on(1, KROBOTIKROBOTICS_FORWARD, 10);
+  * @endcode
+  */
+//% motor.min=1 speed.max=4
+//% speed.min=0 speed.max=100
 void KitronikRobotics::motor_on(uint8_t motor, uint8_t direction, uint16_t speed){
     uint8_t value_buf[4];
     uint8_t forward_offset[4] = {0, 1, 4, 5};
@@ -82,8 +145,12 @@ void KitronikRobotics::motor_on(uint8_t motor, uint8_t direction, uint16_t speed
     uint8_t *offset_buf;
     uint8_t motor_reg;
     uint16_t output_value;
+
     if (!(status & KITRONIKROBOTICS_INITIALIZED))
         init();
+
+    motor = clamp(motor, (uint8_t) 1, (uint8_t) 4);
+    speed = clamp(speed, (uint16_t) 0, (uint16_t) 100);
 
     motor_reg = mot_reg_base + (2 * (motor - 1) * reg_offset);
     output_value = speed * 40;
@@ -98,5 +165,89 @@ void KitronikRobotics::motor_on(uint8_t motor, uint8_t direction, uint16_t speed
     }
     for (uint8_t i = 0; i < 4; i++) {
         i2c.writeRegister(chip_address, motor_reg + offset_buf[i], value_buf[i]);
+    }
+}
+
+/**
+ * Turns off the specified motor.
+ * @param motor which motor to turn off
+ */
+//% motor.min=1 speed.max=4
+void KitronikRobotics::motor_off(uint8_t motor){
+    motor_on(motor, KITRONIKROBOTICS_FORWARD, 0);
+}
+
+/**
+ * Turns off all motors and servos.
+ */
+void KitronikRobotics::all_off(){
+    for (uint8_t motor_i = 1; motor_i < 5; motor_i++) {
+        motor_off(motor_i);
+    }
+
+    for (uint8_t servo_i = 1; servo_i < 5; servo_i++) {
+        servo_write(servo_i, 0);
+    }
+}
+
+/**
+ * Sets the requested stepper motor to a chosen angle relative to the start position.
+ * if the PCA has not yet been initialised calls the initialisation routine.
+ * @param stepper which stepper motor to turn on
+ * @param direction   which direction to go
+ * @param angle how far to turn the motor relative to start in degrees
+ */
+//% stepper.min=1 stepper.max=2
+//% angle.min=1 angle.max=360
+void KitronikRobotics::stepper_motor_turn_angle(uint8_t stepper, uint8_t direction, uint16_t angle) {
+    uint8_t stepper_i;
+    stepper_i  = clamp(stepper, (uint8_t) 1, (uint8_t) 2) - 1;
+    uint16_t steps;
+
+    steps = angle_to_steps(angle, stepper_steps[stepper_i]);
+    stepper_motor_turn_steps(stepper, direction, steps);
+}
+
+/**
+ * Sets the requested stepper motor to turn a set number of steps.
+ * if the PCA has not yet been initialised calls the initialisation routine.
+ * @param stepper which stepper motor to turn on
+ * @param direction   which direction to go
+ * @param steps how many steps to turn the motor
+ */
+    //% stepper.min=1 stepper.max=2
+void KitronikRobotics::stepper_motor_turn_steps(uint8_t stepper, uint8_t direction, uint16_t steps) {
+    uint8_t motors[2][2] = {{1,2},{3,4}};
+    uint8_t motor;
+    uint8_t stepper_i;
+    uint8_t current_direction;
+    uint16_t step_stage = 0;
+
+    stepper_i  = clamp(stepper, (uint8_t) 1, (uint8_t) 2) - 1;
+
+    if (!(status & KITRONIKROBOTICS_INITIALIZED))
+        init();
+
+    for (uint16_t step_counter = 0; step_counter < steps; step_counter ++) {
+        /* step_stage % 2 speedup by using binary & 0x01 */
+        if ((step_stage & 0x01) == 0) {
+            motor = motors[stepper_i][0];
+        } else {
+            motor = motors[stepper_i][1];
+        }
+        if (step_stage == 0 || step_stage == 3) {
+            current_direction = KITRONIKROBOTICS_FORWARD;
+        } else {
+            current_direction = KITRONIKROBOTICS_REVERSE;
+        }
+        motor_on(motor, current_direction, 100);
+        fiber_sleep(20);
+
+        if (direction == KITRONIKROBOTICS_FORWARD) {
+            step_stage++;
+        } else {
+            step_stage--;
+        }
+        step_stage %= 4;
     }
 }
